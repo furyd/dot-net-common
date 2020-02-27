e8c7eb5eb73e5eaf522c49bb7f88de190173e081
Dave Fury <david.fury@reservoir-devs.com>
2020-02-27 12:04:20 +0000

    tests: updated extension tests to add in a test for when additional data is passed

--- tests/unit tests/ReservoirDevs.Common.Extensions.Tests/Logging/ILoggerExtensions/ILoggerExtensionsTests.cs
+++ tests/unit tests/ReservoirDevs.Common.Extensions.Tests/Logging/ILoggerExtensions/ILoggerExtensionsTests.cs
@@ -30,18 +30,44 @@ namespace ReservoirDevs.Common.Extensions.Tests.Logging.ILoggerExtensions
         }
 
         [Fact]
-        public void BeginScope_ShouldCallBeginScope_WithCorrectData()
+        public void BeginScope_ShouldCallBeginScopeWithCorrectData()
         {
             _logger.Setup(logger => logger.BeginScope(It.IsAny<IDictionary<string, string>>())).Returns(new Mock<IDisposable>().Object);
 
             var data = new Dictionary<string, string>
             {
-                {"class", typeof(ILoggerExtensionsTests).FullName}, {"method", nameof(BeginScope_ShouldCallBeginScope_WithCorrectData)}
+                {"class", typeof(ILoggerExtensionsTests).FullName}, {"method", nameof(BeginScope_ShouldCallBeginScopeWithCorrectData)}
             };
 
-            var sut = _logger.Object.CreateScope(nameof(BeginScope_ShouldCallBeginScope_WithCorrectData));
+            var sut = _logger.Object.CreateScope(nameof(BeginScope_ShouldCallBeginScopeWithCorrectData));
 
             _logger.Verify(logger => logger.BeginScope(It.Is<IDictionary<string, string>>(dictionary => dictionary.SequenceEqual(data))), Times.Once);
+        }
+
+        [Fact]
+        public void BeginScope_ShouldCallBeginScopeWithCorrectData_WhenPassesExtraInformation()
+        {
+            _logger.Setup(logger => logger.BeginScope(It.IsAny<IDictionary<string, string>>())).Returns(new Mock<IDisposable>().Object);
+
+            var extraData = new Dictionary<string, string>
+            {
+                { "a", "1" },
+                { "b", "2" }
+            };
+
+            var data = new Dictionary<string, string>
+            {
+                {"class", typeof(ILoggerExtensionsTests).FullName}, {"method", nameof(BeginScope_ShouldCallBeginScopeWithCorrectData_WhenPassesExtraInformation)}
+            };
+
+            var expected = new Dictionary<string, string>();
+
+            extraData.ToList().ForEach(item => expected.Add(item.Key, item.Value));
+            data.ToList().ForEach(item => expected.Add(item.Key, item.Value));
+
+            var sut = _logger.Object.CreateScope(nameof(BeginScope_ShouldCallBeginScopeWithCorrectData_WhenPassesExtraInformation), extraData);
+
+            _logger.Verify(logger => logger.BeginScope(It.Is<IDictionary<string, string>>(dictionary => dictionary.SequenceEqual(expected))), Times.Once);
         }
     }
 }
